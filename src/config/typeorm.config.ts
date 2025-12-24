1c37fa56de17bfe5fef86babda82c4e954e5c53b
Karam Afandi <karam_afandi@yahoo.com>
2025-12-24 14:28:37 +0400

    prod change creds

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,11 +3,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'dpg-d55pttf5r7bs73f6mrk0-a',
   port: parseInt(process.env.DB_PORT || '5432', 10),
-  username: process.env.DB_USERNAME || 'karamafandi',
-  password: process.env.DB_PASSWORD || 'postgres',
-  database: process.env.DB_DATABASE || 'dental_db',
+  username: 'dental_db_1gzr_user',
+  password: 'Spoe2MiynEabsHnxVnI2Bhmu7XoB8lnL',
+  database: 'postgresql://dental_db_1gzr_user:Spoe2MiynEabsHnxVnI2Bhmu7XoB8lnL@dpg-d55pttf5r7bs73f6mrk0-a/dental_db_1gzr',
   autoLoadEntities: true,
   synchronize: process.env.NODE_ENV !== 'production',
   ssl:
